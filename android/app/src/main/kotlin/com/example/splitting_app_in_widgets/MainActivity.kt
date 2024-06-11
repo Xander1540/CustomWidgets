1b6199389d6f6f4d7f78b98ea4087a58d3ff8fab
Xander1540 <adityaxxx2003@gmail.com>
2024-06-11 23:13:05 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/splitting_app_in_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.splitting_app_in_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var arrColors = [Colors.amberAccent,
+  Colors.blue,
+  Colors.blueGrey,
+  Colors.cyan,
+  Colors.deepOrange,
+  Colors.deepPurple,
+  Colors.green,
+  Colors.indigo,
+  Colors.lightBlue,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.orange,
+  Colors.pink,
+  Colors.purple,
+  Colors.red,
+  Colors.teal,
+  Colors.yellow,
+    Colors.lightBlue,];
+
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Custom Widgets'),
+      ),
+      body: Container(
+          child: Column(
+            children: [
+
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.greenAccent,
+                  child: ListView.builder(itemBuilder: (context,index) =>
+                      const Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/images/icon.jpg'),
+                          radius: 50,
+                        ),
+                      ),itemCount: 10, scrollDirection: Axis.horizontal,),
+                ),
+              ),
+
+              Expanded(
+                flex: 4,
+                child: Container(
+                    color: Colors.blueAccent,
+                  child: ListView.builder(itemBuilder: (context, index){
+                    return ListTile(
+                      leading: Image.asset('assets/images/person.png'),
+                      title: Text('Name'),
+                      subtitle: Text('Phone Number'),
+                      trailing: Icon(Icons.delete_forever),
+                    );
+                  }, itemCount: 15,),
+                ),
+              ),
+
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.orangeAccent,
+                  child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.red.shade100,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.red.shade200,),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                              width: 100,
+                            height: 100,
+                            color: Colors.red.shade300),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            color: Colors.red.shade400),
+                        )
+                      ],),
+                  ),
+                ),
+
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.grey,
+                  child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 6,
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10),
+                  itemBuilder: (context, index){
+                    return Container(
+                      color: arrColors[index]);
+                  }, itemCount: arrColors.length)
+                ),
+              ),
+            ],
+          ),
+      ),
+   );
+  }
+}
