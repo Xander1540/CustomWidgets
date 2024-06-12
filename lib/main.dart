61a5542f4dc370a818f18472e2150628a7a2e521
Xander1540 <adityaxxx2003@gmail.com>
2024-06-12 11:12:53 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -33,26 +33,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  var arrColors = [Colors.amberAccent,
-  Colors.blue,
-  Colors.blueGrey,
-  Colors.cyan,
-  Colors.deepOrange,
-  Colors.deepPurple,
-  Colors.green,
-  Colors.indigo,
-  Colors.lightBlue,
-  Colors.lightGreen,
-  Colors.lime,
-  Colors.orange,
-  Colors.pink,
-  Colors.purple,
-  Colors.red,
-  Colors.teal,
-  Colors.yellow,
-    Colors.lightBlue,];
-
-
   @override
   Widget build(BuildContext context) {
    return Scaffold(
@@ -64,93 +44,141 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             children: [
 
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.greenAccent,
-                  child: ListView.builder(itemBuilder: (context,index) =>
-                      const Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: CircleAvatar(
-                          backgroundImage: AssetImage('assets/images/icon.jpg'),
-                          radius: 50,
-                        ),
-                      ),itemCount: 10, scrollDirection: Axis.horizontal,),
-                ),
-              ),
+              ListViewA(),
 
-              Expanded(
-                flex: 4,
-                child: Container(
-                    color: Colors.blueAccent,
-                  child: ListView.builder(itemBuilder: (context, index){
-                    return ListTile(
-                      leading: Image.asset('assets/images/person.png'),
-                      title: Text('Name'),
-                      subtitle: Text('Phone Number'),
-                      trailing: Icon(Icons.delete_forever),
-                    );
-                  }, itemCount: 15,),
-                ),
-              ),
+              ListViewB(),
 
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.orangeAccent,
-                  child: ListView(
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.red.shade100,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.red.shade200,),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                              width: 100,
-                            height: 100,
-                            color: Colors.red.shade300),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            color: Colors.red.shade400),
-                        )
-                      ],),
-                  ),
-                ),
+              ListViewC(),
 
-              Expanded(
-                flex: 2,
-                child: Container(
-                  color: Colors.grey,
-                  child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 6,
-                    mainAxisSpacing: 10,
-                    crossAxisSpacing: 10),
-                  itemBuilder: (context, index){
-                    return Container(
-                      color: arrColors[index]);
-                  }, itemCount: arrColors.length)
-                ),
-              ),
+              GridViewA(),
             ],
           ),
       ),
    );
+  }
+}
+
+class ListViewA extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Colors.greenAccent,
+        child: ListView.builder(itemBuilder: (context,index) =>
+        const Padding(
+          padding: EdgeInsets.all(10.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage('assets/images/icon.jpg'),
+            radius: 50,
+          ),
+        ),itemCount: 10, scrollDirection: Axis.horizontal,),
+      ),
+    );
+  }
+}
+
+class ListViewB extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Expanded(
+      flex: 4,
+      child: Container(
+        color: Colors.blueAccent,
+        child: ListView.builder(itemBuilder: (context, index){
+          return ListTile(
+            leading: Image.asset('assets/images/person.png'),
+            title: Text('Name'),
+            subtitle: Text('Phone Number'),
+            trailing: Icon(Icons.delete_forever),
+          );
+        }, itemCount: 15,),
+      ),
+    );
+  }
+}
+
+class ListViewC extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Colors.orangeAccent,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red.shade100,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.red.shade200,),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red.shade300),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red.shade400),
+            )
+          ],),
+      ),
+    );
+  }
+}
+
+class GridViewA extends StatelessWidget{
+
+  var arrColors = [Colors.amberAccent,
+    Colors.blue,
+    Colors.blueGrey,
+    Colors.cyan,
+    Colors.deepOrange,
+    Colors.deepPurple,
+    Colors.green,
+    Colors.indigo,
+    Colors.lightBlue,
+    Colors.lightGreen,
+    Colors.lime,
+    Colors.orange,
+    Colors.pink,
+    Colors.purple,
+    Colors.red,
+    Colors.teal,
+    Colors.yellow,
+    Colors.lightBlue,];
+
+  @override
+  Widget build(BuildContext context){
+    return Expanded(
+      flex: 2,
+      child: Container(
+          color: Colors.grey,
+          child: GridView.builder(gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 6,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10),
+              itemBuilder: (context, index){
+                return Container(
+                    color: arrColors[index]);
+              }, itemCount: arrColors.length)
+      ),
+    );
   }
 }
